e72ab9d2e0f06ea1b71b4a71d97a765baacf8369
nwjbrandon <nwjbrandon@gmail.com>
2020-11-18 11:22:43 +0800

    add ddqn quantized

--- src/convertor.py
+++ src/convertor.py
@@ -1,9 +1,10 @@
 from dqn import DQNAgent
+from ddqn import DoubleDQNAgent
 
 import tensorflow_model_optimization as tfmot
 import tensorflow as tf
 
-agent = DQNAgent(4,2,load_model=True)
+agent = DoubleDQNAgent(4,2,load_model=True)
 
 print(agent.model)
 
@@ -11,5 +12,5 @@ converter = tf.lite.TFLiteConverter.from_keras_model(agent.model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 quantized_tflite_model = converter.convert()
 
-with open('cartpole_dqn.tflite', 'wb') as f:
+with open('cartpole_ddqn.tflite', 'wb') as f:
     f.write(quantized_tflite_model)
